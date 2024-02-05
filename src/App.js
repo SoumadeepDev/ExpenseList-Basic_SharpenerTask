1c9d6f8e8f3fff5f9d21e818e8b798f3f37e1ea4
Soumadeep Chatterjee <139675958+SoumadeepDev@users.noreply.github.com>
2024-02-05 16:38:13 +0530

    Add Title Change and Delete Functionality

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 import { useState } from "react";
 import { EXPENSE_DATA } from "./data";
+import card from "./components/card";
 function App() {
   const [expenses, setExpenses] = useState(EXPENSE_DATA);
+
+  const handleDeleteExpense = (id) => {
+    setExpenses((prevExpenses) =>
+      prevExpenses.filter((expense) => expense.id !== id)
+    );
+  };
   return (
     <div>
       <h1>Expense Tracker</h1>
       {expenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
+          id={expense.id}
           date={expense.date}
-          item={expense.item}
+          title={expense.title}
           location={expense.LocationOfExpenditure}
           price={expense.price}
+          onDelete={handleDeleteExpense}
         />
       ))}
     </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,60 @@
 import "./ExpenseItem.css";
+import card from "./card";
 import ExpenseDate from "./ExpenseDate.js";
+import { useState } from "react";
+
 function ExpenseItem(props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(props.title);
+
+  function handleTitleClick() {
+    setIsEditing(true);
+  }
+  function handleSaveClick() {
+    setIsEditing(false);
+  }
+  function handleInputChange(event) {
+    setEditedTitle(event.target.value);
+  }
+
+  function handleDelete() {
+    props.onDelete(props.id);
+  }
+
   return (
-    <div className="expense-item">
+    <card className="expense-item expenses">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.item}</h2>
-        <h2 className="Location">{`Location: ${props.location}`}</h2>
+        {!isEditing ? (
+          <h2>{editedTitle}</h2>
+        ) : (
+          <input
+            type="text"
+            className="input-title"
+            value={editedTitle}
+            onChange={handleInputChange}
+          />
+        )}
+
+        <h2 className="Location">{props.location}</h2>
         <div className="expense-item__price">${props.price}</div>
+
+        {!isEditing && (
+          <button className="change_button" onClick={handleTitleClick}>
+            Change Title
+          </button>
+        )}
+        {isEditing && (
+          <button className="change_button" onClick={handleSaveClick}>
+            Save
+          </button>
+        )}
+
+        <button onClick={handleDelete} className="delete_button">
+          Delete
+        </button>
       </div>
-    </div>
+    </card>
   );
 }
 
